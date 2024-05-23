740bc96d1d86c8a1bd891d2aceb71dc0be680d7c
MGold <mikkelgold@protonmail.com>
2024-05-23 15:59:49 +0200

    hh

--- src/main/java/app/model/Role.java
+++ src/main/java/app/model/Role.java
@@ -14,9 +14,13 @@ import java.util.Set;
 @Entity
 @NoArgsConstructor
 @AllArgsConstructor
-@Table(name = "role")
+@Table(name = "roles")
 public class Role {
+
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
     @Column(name = "rolename", nullable = false)
     private String rolename;
 
