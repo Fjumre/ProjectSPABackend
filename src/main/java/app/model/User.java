1171eb8902d23757aa342c8103a3d009d09af20a
MGold <mikkelgold@protonmail.com>
2024-05-24 12:48:07 +0200

    ProjectBackend

--- src/main/java/app/model/User.java
+++ src/main/java/app/model/User.java
@@ -19,7 +19,7 @@ import java.util.Set;
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name= "id", nullable = false, unique = true)
+    @Column(name = "id", nullable = false, unique = true)
     private int id;
 
     @Column(name = "username", nullable = false, unique = true)
