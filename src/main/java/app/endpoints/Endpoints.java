9739355b69e02187bf22a8c65a99475b7969e505
MGold <mikkelgold@protonmail.com>
2024-05-28 12:56:50 +0200

    ProjectBackend

--- src/main/java/app/endpoints/Endpoints.java
+++ src/main/java/app/endpoints/Endpoints.java
@@ -47,7 +47,6 @@ public class Endpoints {
                 get("/list", toDoController.getAllToDos(), RouteRoles.USER, RouteRoles.ADMIN);
                 get("/list/{id}", toDoController.getToDoById(), RouteRoles.USER, RouteRoles.ADMIN);
                 get("/list/{date}", toDoController.getToDoByDate(), RouteRoles.USER, RouteRoles.ADMIN);
-                put("/list/update/{id}/date/{date}", toDoController.updateToDoByIdAndDate(), RouteRoles.USER, RouteRoles.ADMIN);
                 put("/list/update/{id}", toDoController.updateToDo(), RouteRoles.USER, RouteRoles.ADMIN);
                 delete("/list/delete/{id}", toDoController.deleteToDo(), RouteRoles.USER, RouteRoles.ADMIN);
                 post("/list/create", toDoController.createToDo(), RouteRoles.USER, RouteRoles.ADMIN);
